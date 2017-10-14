f76c25c939e5ac263a17819d877ba97fa0f630bf
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-10-14 19:09:38 +0300

    Provide ability to keep and restore state

--- il2fb/ds/airbridge/watchdog.py
+++ il2fb/ds/airbridge/watchdog.py
@@ -7,7 +7,7 @@ import time
 
 from pathlib import Path
 
-from .typing import StringHandler, StringOrPath
+from .typing import StringHandler, StringOrPath, IntOrNone
 
 
 LOG = logging.getLogger(__name__)
@@ -19,8 +19,16 @@ class StopWatchdog(Exception):
 
 class TextFileWatchdog:
 
-    def __init__(self, path: StringOrPath, polling_period: float=0.5):
+    def __init__(
+        self,
+        path: StringOrPath,
+        inode: IntOrNone=None,
+        offset: int=0,
+        polling_period: float=0.5,
+    ):
         self._path = path if isinstance(path, Path) else Path(path)
+        self._inode = inode
+        self._offset = offset
         self._polling_period = polling_period
 
         self._do_stop = False
@@ -28,8 +36,6 @@ class TextFileWatchdog:
 
         self._subscribers = []
         self._subscribers_lock = threading.Lock()
-
-        self._inode = None
 
     def subscribe(self, subscriber: StringHandler) -> None:
         with self._subscribers_lock:
@@ -46,46 +52,85 @@ class TextFileWatchdog:
     def run(self) -> None:
         try:
             LOG.info(f"watchdog for text file `{self._path}` has started")
-            self._try_run()
+            self._run_with_retries()
         except StopWatchdog:
             LOG.info(f"watchdog for text file `{self._path}` has stopped")
         except Exception:
             LOG.info(f"watchdog for text file `{self._path}` has terminated")
             raise
 
-    def _try_run(self) -> None:
+    def _run_with_retries(self) -> None:
         while True:
             self._maybe_stop()
 
             try:
-                self._run()
+                self._try_to_run()
             except FileNotFoundError:
+                self._clear_state()
                 continue
 
-    def _run(self) -> None:
+    def _try_to_run(self) -> None:
+        if self._path.exists():
+            self._reset_state_if_file_was_recreated()
+        else:
+            self._clear_state()
+            self._wait_for_file_to_get_created()
+
+        if self._inode is None:
+            self._inode = self._get_actual_inode()
+
+        with self._path.open(buffering=1) as f:
+            f.seek(self._offset)
+            self._read_lines(f)
+
+    def _reset_state_if_file_was_recreated(self):
+        inode = self._get_actual_inode()
+
+        if self._inode != inode:
+            self._inode = inode
+            self._offset = 0
+
+    def _clear_state(self) -> None:
+        self._inode = None
+        self._offset = 0
+
+    def _wait_for_file_to_get_created(self):
         while not self._path.exists():
             self._sleep_and_maybe_stop()
 
-        with self._path.open(buffering=1) as f:
-            self._read_lines(f)
-
     def _read_lines(self, f: io.TextIOWrapper) -> None:
-        self._inode = self._get_inode()
-
         while True:
-            where = f.tell()
+            self._offset = f.tell()
             line = f.readline()
 
             if line:
                 line = line.strip()
                 self._handle_string(line)
             else:
-                self._check_if_file_is_still_the_same()
+                self._stop_if_file_was_deleted_or_recreated()
                 self._sleep_and_maybe_stop()
-                f.seek(where)
+                f.seek(self._offset)
 
-    def _get_inode(self):
+    def _stop_if_file_was_deleted_or_recreated(self) -> None:
+        if not self._file_is_still_the_same():
+            raise FileNotFoundError
+
+    def _file_is_still_the_same(self) -> bool:
+        inode = self._get_actual_inode()
+        return (self._inode == inode)
+
+    def _get_actual_inode(self) -> int:
+        # Can raise FileNotFoundError if file does not exist
         return self._path.lstat().st_ino
+
+    def _sleep_and_maybe_stop(self) -> None:
+        time.sleep(self._polling_period)
+        self._maybe_stop()
+
+    def _maybe_stop(self) -> None:
+        with self._stop_lock:
+            if self._do_stop:
+                raise StopWatchdog
 
     def _handle_string(self, s: str) -> None:
         with self._subscribers_lock:
@@ -96,23 +141,3 @@ class TextFileWatchdog:
                     LOG.exception(
                         f"subscriber {subscriber} failed to handle string {s}"
                     )
-
-    def _check_if_file_is_still_the_same(self):
-        try:
-            inode = self._get_inode()
-        except FileNotFoundError:
-            self._inode = None
-            raise
-
-        if inode != self._inode:
-            self._inode = None
-            raise FileNotFoundError
-
-    def _sleep_and_maybe_stop(self) -> None:
-        time.sleep(self._polling_period)
-        self._maybe_stop()
-
-    def _maybe_stop(self) -> None:
-        with self._stop_lock:
-            if self._do_stop:
-                raise StopWatchdog
