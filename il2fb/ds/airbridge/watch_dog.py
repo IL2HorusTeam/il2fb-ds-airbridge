bc69d1a47a4994071d0b46c7dd29aebf6a7d4169
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-10-21 14:11:19 +0300

    Fix stopping of TextFileWatchDog

--- il2fb/ds/airbridge/watch_dog.py
+++ il2fb/ds/airbridge/watch_dog.py
@@ -60,7 +60,7 @@ class TextFileWatchDog:
 
     def stop(self) -> None:
         with self._stop_lock:
-            self._do_stop = False
+            self._do_stop = True
 
     def run(self) -> None:
         try:
