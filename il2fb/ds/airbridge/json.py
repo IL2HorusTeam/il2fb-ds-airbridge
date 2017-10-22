b5f758d78ff4242578a77fdfd18339a31845293a
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-10-22 17:44:10 +0300

    Update custom JSON encoder

--- il2fb/ds/airbridge/json.py
+++ il2fb/ds/airbridge/json.py
@@ -4,6 +4,8 @@ import json as _json
 
 from functools import partial
 
+from il2fb.commons.events import Event
+
 
 __all__ = ('dumps', 'loads', )
 
@@ -12,7 +14,15 @@ class JSONEncoder(_json.JSONEncoder):
 
     def default(self, obj):
         if hasattr(obj, 'to_primitive'):
+            cls = obj.__class__
+
             result = obj.to_primitive()
+            result['__type__'] = f"{cls.__module__}.{cls.__name__}"
+
+            if issubclass(cls, Event):
+                result.pop('name')
+                result.pop('verbose_name')
+
         elif hasattr(obj, 'isoformat'):
             result = obj.isoformat()
         else:
