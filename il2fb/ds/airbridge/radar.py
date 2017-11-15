af5b990c4c2764fa60ddb6bb2d644a5a6f39ead4
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-11-15 23:32:11 +0200

    Add timeouts for radar

--- il2fb/ds/airbridge/radar.py
+++ il2fb/ds/airbridge/radar.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import logging
+import time
 
 from typing import Awaitable, List
 
@@ -48,62 +49,96 @@ class Radar:
 
     async def get_moving_ships_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.ShipPosition]]:
 
         await self._client.refresh_radar()
-        return (await self._get_moving_ships_positions())
+        return (await self._get_moving_ships_positions(timeout))
 
     async def _get_moving_ships_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.ShipPosition]]:
 
-        ships = await self._client.get_all_ships_positions()
+        ships = await self._client.get_all_ships_positions(timeout)
         return [ship for ship in ships if not ship.is_stationary]
 
     async def get_stationary_ships_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.ShipPosition]]:
 
         await self._client.refresh_radar()
-        return (await self._get_stationary_ships_positions())
+        return (await self._get_stationary_ships_positions(timeout))
 
     async def _get_stationary_ships_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.ShipPosition]]:
 
-        ships = await self._client.get_all_ships_positions()
+        ships = await self._client.get_all_ships_positions(timeout)
         return [ship for ship in ships if ship.is_stationary]
 
     async def get_all_ships_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.ShipPosition]]:
 
         await self._client.refresh_radar()
-        return (await self._client.get_all_ships_positions())
+        return (await self._client.get_all_ships_positions(timeout))
 
     async def get_moving_aircrafts_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.MovingAircraftPosition]]:
 
         await self._client.refresh_radar()
-        return (await self._client.get_all_moving_aircrafts_positions())
+        return (await self._client.get_all_moving_aircrafts_positions(timeout))
 
     async def get_moving_ground_units_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.MovingGroundUnitPosition]]:
 
         await self._client.refresh_radar()
-        return (await self._client.get_all_moving_ground_units_positions())
+        return (await self._client.get_all_moving_ground_units_positions(
+            timeout=timeout,
+        ))
 
     async def get_all_moving_actors_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[AllMovingActorsPositions]:
 
         await self._client.refresh_radar()
 
-        aircrafts = await self._client.get_all_moving_aircrafts_positions()
-        ground_units = await self._client.get_all_moving_ground_units_positions()
-        ships = await self._get_moving_ships_positions()
+        if timeout is None:
+            aircrafts = await self._client.get_all_moving_aircrafts_positions()
+            ground_units = await self._client.get_all_moving_ground_units_positions()
+            ships = await self._get_moving_ships_positions()
+        else:
+            start_time = time.monotonic()
+            aircrafts = await self._client.get_all_moving_aircrafts_positions(
+                timeout=timeout,
+            )
+
+            end_time = time.monotonic()
+            timeout -= (end_time - start_time)
+            start_time = end_time
+            if timeout <= 0:
+                raise TimeoutError
+
+            ground_units = await self._client.get_all_moving_ground_units_positions(
+                timeout=timeout,
+            )
+
+            end_time = time.monotonic()
+            timeout -= (end_time - start_time)
+            start_time = end_time
+            if timeout <= 0:
+                raise TimeoutError
+
+            ships = await self._get_moving_ships_positions(timeout)
 
         return AllMovingActorsPositions(
             aircrafts=aircrafts,
@@ -113,27 +148,54 @@ class Radar:
 
     async def get_stationary_objects_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.StationaryObjectPosition]]:
 
         await self._client.refresh_radar()
-        return (await self._client.get_all_stationary_objects_positions())
+        return (await self._client.get_all_stationary_objects_positions(
+            timeout=timeout,
+        ))
 
     async def get_all_houses_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[List[structures.HousePosition]]:
 
         await self._client.refresh_radar()
-        return (await self._client.get_all_houses_positions())
+        return (await self._client.get_all_houses_positions(timeout))
 
     async def get_all_stationary_actors_positions(
         self,
+        timeout: float=None,
     ) -> Awaitable[AllStationaryActorsPositions]:
 
         await self._client.refresh_radar()
 
-        stationary_objects = await self._client.get_all_stationary_objects_positions()
-        houses = await self._client.get_all_houses_positions()
-        ships = await self._get_stationary_ships_positions()
+        if timeout is None:
+            stationary_objects = await self._client.get_all_stationary_objects_positions()
+            houses = await self._client.get_all_houses_positions()
+            ships = await self._get_stationary_ships_positions()
+        else:
+            start_time = time.monotonic()
+            stationary_objects = await self._client.get_all_stationary_objects_positions(
+                timeout=timeout,
+            )
+
+            end_time = time.monotonic()
+            timeout -= (end_time - start_time)
+            start_time = end_time
+            if timeout <= 0:
+                raise TimeoutError
+
+            houses = await self._client.get_all_houses_positions(timeout)
+
+            end_time = time.monotonic()
+            timeout -= (end_time - start_time)
+            start_time = end_time
+            if timeout <= 0:
+                raise TimeoutError
+
+            ships = await self._get_stationary_ships_positions(timeout)
 
         return AllStationaryActorsPositions(
             stationary_objects=stationary_objects,
