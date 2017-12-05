f7ca94e0dbc261799054758c674a9b4203f11194
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-12-05 13:58:01 +0200

    Set package version to 1.0.0

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ REQUIREMENTS, DEPENDENCIES = parse_requirements(REQUIREMENTS_FILE_PATH)
 
 setup(
     name="il2fb-ds-airbridge",
-    version="1.0.0-rc",
+    version="1.0.0",
     description=(
         "Wrapper of dedicated server of «IL-2 Sturmovik: Forgotten Battles»"
     ),
@@ -110,7 +110,7 @@ setup(
         "Operating System :: Unix",
         "Operating System :: Microsoft :: Windows",
         "License :: OSI Approved :: MIT License",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Communications",
         "Topic :: Games/Entertainment :: Simulation",
         "Environment :: Console",
